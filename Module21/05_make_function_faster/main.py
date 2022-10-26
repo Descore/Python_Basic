7c7f01d7450b05ef0bd5db371b0a70a3ec9e98ae
Timofey <45923293+Descore@users.noreply.github.com>
2022-10-26 12:52:33 +0300

    Delete Module21 directory

--- Module21/05_make_function_faster/main.py
+++
@@ -1,9 +0,0 @@
-def calculating_math_func(data):
-    result = 1
-    for index in range(1, data + 1):
-        result *= index
-    result /= data ** 3
-    result = result ** 10
-    return result
-
-# TODO оптимизировать функцию
